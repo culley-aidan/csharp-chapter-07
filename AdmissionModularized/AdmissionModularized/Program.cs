6e14e491ffbeb9265fc34fee000d695d24b2cc75
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-18 09:04:04 -0600

    finished

---
+++ AdmissionModularized/AdmissionModularized/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AdmissionModularized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your GPA: ");
+            double.TryParse(Console.ReadLine(), out double enteredGPA);
+            Console.Write("Enter your admission test score: ");
+            int.TryParse(Console.ReadLine(), out int enteredScore);
+            Console.WriteLine(Admission(enteredGPA, enteredScore));
+        }
+        static string Admission(double enteredGPA, int enteredScore)
+        {
+            string admission = "";
+            if ((enteredGPA < 3.0D && enteredScore >= 80) || (enteredGPA >= 3.0D && enteredScore >= 60))
+            {
+                admission = "Accept";
+            } else
+            {
+                admission = "Reject";
+            }
+            return admission;
+        }
+    }
+}
